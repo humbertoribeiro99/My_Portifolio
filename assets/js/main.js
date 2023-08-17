a34a260f6cff3447f45d492dc9bd6dc215e068bf
Humberto Eduardo Ribeiro da Silva <humberto.silva@corendf.gov.br>
2023-08-17 16:56:03 -0300

    Atualizalição no codigo

--- assets/js/main.js
+++ assets/js/main.js
@@ -51,7 +51,25 @@ skillsHeader.forEach((el) =>{
 })
 
 /*==================== QUALIFICATION TABS ====================*/
+const tabs = document.querySelectorAll('[data-target]'),
+      tabContents = document.querySelectorAll('[data-target]')  
 
+tabs.forEach(tab =>{
+    tab.addEventListener('click', () =>{
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContent =>{
+            tabContent.classList.remove('qualification_active')
+        })
+        target.classList.add('qualification_active')
+
+        tabs.forEach(tab =>{
+            tab.classList.remove('qualification_active')
+        })
+        tab.classList.add('qualification_active')
+    })
+
+})
 
 /*==================== SERVICES MODAL ====================*/
 
